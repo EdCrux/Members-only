52c574b4b766e998317aa6bdacc63dd8e682cc47
Eduardo <jecruxiz@gmail.com>
2020-06-01 16:18:09 -0500

    :sparkles: Add index, new, create actions for posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,27 @@ class PostsController < ApplicationController
 before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    
+    @posts = Post.all.order('created_at DESC')
+    render json: @posts
   end
 
+  def new
+    @post = current_user.posts.new
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+
+    if @post.save
+      flash[:notice] = "Post created successfully "
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+   def post_params
+    params.require(:post).permit(:description)
+   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_01_203939) do
+ActiveRecord::Schema.define(version: 2020_06_01_210223) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text "description"
+    t.integer "users_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["users_id"], name: "index_posts_on_users_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
@@ -25,4 +33,5 @@ ActiveRecord::Schema.define(version: 2020_06_01_203939) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users", column: "users_id"
 end
