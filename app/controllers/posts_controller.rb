19a8770581a2b4d17e182adb7327d626ddcefd5f
Eduardo <jecruxiz@gmail.com>
2020-06-01 18:17:07 -0500

    :bug: Fix form action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-before_action :authenticate_user!, except: [:index, :show]
+before_action :authenticate_user!
 
   def index
     @posts = Post.all.order('created_at DESC')
