bbabf4b68e6bd9d501a521645745c4ef347d1292
kitpao <kitpao91@gmail.com>
2020-06-02 16:38:27 -0500

    :lipstick: Change style for pagination bar

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,9 +2,7 @@ class PostsController < ApplicationController
 before_action :authenticate_user!, except: [:index]
 
   def index
-    @posts = Post.paginate(page: params[:page], per_page: 5)
-    #@posts= Post.page(params[:page]).order('created_at DESC')
-    #@posts = Post.paginate(page: params[:page])
+    @posts = Post.paginate(page: params[:page], per_page: 5).order('created_at DESC')
 
     if user_signed_in?
       @post = current_user.posts.build
