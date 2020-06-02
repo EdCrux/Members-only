a8d07395a4a64cfb3304ab04f18640a4fee68613
Eduardo <jecruxiz@gmail.com>
2020-06-02 17:30:32 -0500

    :rotating_light: Remove linter warnings

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :posts
-  validates :username, presence: true, 
-            uniqueness: { case_sensitive: false }
+  validates :username, presence: true,
+                       uniqueness: { case_sensitive: false }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
